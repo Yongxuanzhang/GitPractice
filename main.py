ff9ae3372abd6d886bb9927dd65f9d718c029edc
YongxuanZhang <yongxuanzhang1009@gmail.com>
2021-01-19 13:35:45 -0500

    add test5

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from keras.layers import Dense, Conv2D, Flatten,MaxPooling2D
 
 import keras.layers as kl
 
-#made a pr test1234
+#made a pr test12345
 
 #load data
 #mnist=fetch_openml('mnist_784')
